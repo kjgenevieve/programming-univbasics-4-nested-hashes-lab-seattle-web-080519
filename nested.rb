a1b674fc555e091e638c8ef4d8bb7056b1006438
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-18 18:38:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,12 +64,6 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -85,6 +79,17 @@ def adding_matz
           :languages => ["C"]
         }
      }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+
 
     
 end
